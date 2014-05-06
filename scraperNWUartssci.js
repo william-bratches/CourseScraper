542a1aafa41a8cf70c6e9518e65393e8f341fb2e
william-bratches <william.bratches@gmail.com>
2014-05-05 21:45:07 -0400

    update to NWUartsci script

--- scraperNWUartssci.js
+++ scraperNWUartssci.js
@@ -4,23 +4,27 @@ var cheerio = require('cheerio');
 var _ = require ('underscore');
 var JSONArray = [];
 
-var testObj =  { departmentName: 'CS',
-    departmentLink: 'http://collegecatalog.uchicago.edu/thecollege/computerscience/'};
-
+//var testObj =  { departmentName: 'CS',
+//    departmentLink: 'http://www.registrar.northwestern.edu/courses/wcas_ycp.html'};
+"""
+ran into a roadblock with this script. Every single department seems to have its own unique website. I'm going to focus on the other more
+consistent schools at NWU and see if I can find the consolidated course catalog elsewhere -pdf2html seems like a good solution.
+"""
 
 var getDepartments = function() {
 	var departmentObjects = [];
 	var deptObj = {};
 
 	request({
-		uri: "http://collegecatalog.uchicago.edu/"
+		uri: "http://www.registrar.northwestern.edu/courses/wcas_ycp.html"
 		}, function(error, response, body) {
 			var $ = cheerio.load(body);
 
 			$('a').each(function(){
-				console.log(this.attr('href'));
+				var courseLink = this.attr('href');
+				console.log(courseLink);
 				deptObj.departmentName = this.text();
-				deptObj.departmentLink = "http://collegecatalog.uchicago.edu/" + this['0'].attribs.href;
+				deptObj.departmentLink = courseLink;
 				departmentObjects.push(deptObj);
 				deptObj = {};
 
@@ -43,5 +47,5 @@ var getCourses = function(Obj){
 		});
 };
 
-//getDepartments();
-getCourses(testObj);
+getDepartments();
+//getCourses(testObj);
